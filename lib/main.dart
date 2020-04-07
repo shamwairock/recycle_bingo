b743eede470c879f5809bfc0e61676b2fa57fa82
wai rock <shamwairock@gmail.com>
2020-04-07 22:10:06 +0800

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,55 @@ import 'package:recyclebingo/util/trace_logger.dart';
 import 'styles/font_style.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'dart:io';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MaterialApp(
+  home: new MyApp(),
+  theme: ThemeData(
+            brightness: Brightness.light,
+            primaryColor: Colors.orange,
+            accentColor: Colors.orange,
+            appBarTheme: AppBarTheme(
+            textTheme: TextTheme(title:  AppBarTextStyle)
+            )
+        )
+      )
+    );
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     _requestPermission();
-
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.orange,
-        accentColor: Colors.orange,
-        appBarTheme: AppBarTheme(
-            textTheme: TextTheme(title:  AppBarTextStyle)
+    return new SplashScreen(
+        seconds: 5,
+        navigateAfterSeconds: new NavigationView(),
+        title: new Text('Welcome To Recycle Bingo',
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0,
+              color: Colors.white
+            ),
         ),
-      ),
-      home: NavigationView(),
+        //image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+        backgroundColor: Colors.orange,
+        styleTextUnderTheLoader: SplashScreenTitleTextStyle,
+        loadingText: Text('Loading...',
+          style: SplashScreenLoaderTextStyle
+        ),
+        //photoSize: 100.0,
+        //onClick: () => print("Flutter Egypt"),
+        loaderColor: Colors.white,
     );
   }
 
   Future<void> _requestPermission() async {
-
     var locationStatus = await Permission.location.status;
     Logger.write("locationStatus " + locationStatus.toString());
 
@@ -42,3 +65,5 @@ class MyApp extends StatelessWidget {
     }
   }
 }
+
+
--- lib/styles/font_style.dart
+++ lib/styles/font_style.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 const LargeTextSize = 26.0;
 const MediumTextSize = 20.0;
 const BodyTextSize = 14.0;
+const SmallTextSize = 9.0;
 
 const String FontNameDefault = 'Montserrat';
 
@@ -20,6 +22,21 @@ const AppBarTextStyle = TextStyle(
     fontSize: MediumTextSize,
     color: Colors.white
 );
+
+const SplashScreenTitleTextStyle = TextStyle(
+    fontFamily: FontNameDefault,
+    fontWeight: FontWeight.w300,
+    fontSize: MediumTextSize,
+    color: Colors.white
+);
+
+const SplashScreenLoaderTextStyle = TextStyle(
+    fontFamily: FontNameDefault,
+    fontWeight: FontWeight.w300,
+    fontSize: SmallTextSize,
+    color: Colors.white
+);
+
 
 const TitleTextStyle = TextStyle(
     fontFamily: FontNameDefault,
