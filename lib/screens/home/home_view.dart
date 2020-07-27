cf239f40965a730e6e0d9710f768e190ff25cdfb
wai rock <shamwairock@gmail.com>
2020-07-27 20:41:06 +0800

    added app icon

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,37 +1,233 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:recyclebingo/util/trace_logger.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class HomeView extends StatelessWidget{
-
-  static const double _hPadding = 16.0;
+class HomeView extends StatefulWidget{
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('recycle bingo'),
-        ),
-        backgroundColor: const Color(0xffded3ad),
-        body:Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-//              Container(
-//                  padding: const EdgeInsets.fromLTRB(_hPadding, 32.0, _hPadding, 0),
-//                  child:Text('Hello recycle rangers', style: Theme.of(context).textTheme.title)
-//              ),
-//              Container(
-//                  padding: const EdgeInsets.fromLTRB(_hPadding, 0, _hPadding, 0),
-//                  child:Text('One man\'s Trash is another man\'s Treasure', style: Theme.of(context).textTheme.body1)
-//              ),
-            ],
-          ),
+  _HomeViewState createState() => _HomeViewState();
+}
 
-        ),
-    );
+class _HomeViewState extends State<HomeView> {
+
+  static const double CAMERA_ZOOM = 16;
+  static const double CAMERA_TILT = 0;
+  static const double CAMERA_BEARING = 0;
+  static const LatLng SOURCE_LOCATION = LatLng(1.3636,103.8951);
+  static const LatLng DEST_LOCATION = LatLng(1.3696,103.8871);
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  Set<Marker> _markers = Set<Marker>();
+
+  Set<Polyline> _polylines = Set<Polyline>();
+
+  List<LatLng> polylineCoordinates = [];
+
+  PolylinePoints polylinePoints;
+
+  String googleAPIKey ="AIzaSyD5Kb3oJ247289zbfNgJoYJwnODC9lGshI";
+
+  BitmapDescriptor sourceIcon;
+
+  BitmapDescriptor destinationIcon;
+
+  LocationData currentLocation;
+
+  LocationData destinationLocation;
+
+  Location location;
+
+  CameraPosition initialCameraPosition = CameraPosition(
+      zoom: CAMERA_ZOOM,
+      tilt: CAMERA_TILT,
+      bearing: CAMERA_BEARING,
+      target: SOURCE_LOCATION
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    // create an instance of Location
+    location = new Location();
+    polylinePoints = PolylinePoints();
+
+    // subscribe to changes in the user's location
+    // by "listening" to the location's onLocationChanged event
+    location.onLocationChanged.listen((LocationData cLoc) {
+
+      // cLoc contains the lat and long of the
+      // current user's position in real time,
+      // so we're holding on to it
+      currentLocation = cLoc;
+      updatePinOnMap();
+    });
+    // set custom marker pins
+    setSourceAndDestinationIcons();
+    // set the initial location
+    setInitialLocation();
+  }
+
+  void setSourceAndDestinationIcons() async {
+    sourceIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(devicePixelRatio: 2.5),
+        'assets/driving_pin.png');
+
+    destinationIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(devicePixelRatio: 2.5),
+        'assets/destination_map_marker.png');
+  }
+
+  void setInitialLocation() async {
+    // set the initial location by pulling the user's
+    // current location from the location's getLocation()
+    currentLocation = await location.getLocation();
+
+    // hard-coded destination for this example
+    destinationLocation = LocationData.fromMap({
+      "latitude": DEST_LOCATION.latitude,
+      "longitude": DEST_LOCATION.longitude
+    });
   }
 
 
+
+  void setPolylines() async {
+    PointLatLng curr = new PointLatLng(
+        currentLocation.latitude,
+        currentLocation.longitude);
+    PointLatLng dest = new PointLatLng(
+        destinationLocation.latitude,
+        destinationLocation.longitude);
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        googleAPIKey,
+        curr,
+        dest);
+
+
+
+    if(result.points.isNotEmpty){
+      result.points.forEach((PointLatLng point){
+        polylineCoordinates.add(
+            LatLng(point.latitude,point.longitude)
+        );
+      });
+      setState(() {
+        _polylines.add(Polyline(
+            width: 5, // set the width of the polylines
+            polylineId: PolylineId("poly"),
+            color: Color.fromARGB(255, 40, 122, 198),
+            points: polylineCoordinates
+        ));
+      });
+    }
+  }
+
+  void showPinsOnMap() {
+    // get a LatLng for the source location
+    // from the LocationData currentLocation object
+    var pinPosition = LatLng(currentLocation.latitude,
+        currentLocation.longitude);
+    // get a LatLng out of the LocationData object
+    var destPosition = LatLng(destinationLocation.latitude,
+        destinationLocation.longitude);
+    // add the initial source location pin
+    _markers.add(Marker(
+        markerId: MarkerId('sourcePin'),
+        position: pinPosition,
+        icon: sourceIcon
+    ));
+    // destination pin
+    _markers.add(Marker(
+        markerId: MarkerId('destPin'),
+        position: destPosition,
+        icon: destinationIcon
+    ));
+    // set the route lines on the map from source to destination
+    // for more info follow this tutorial
+    setPolylines();
+  }
+
+  void updatePinOnMap() async {
+
+    // create a new CameraPosition instance
+    // every time the location changes, so the camera
+    // follows the pin as it moves with an animation
+    CameraPosition cPosition = CameraPosition(
+      zoom: CAMERA_ZOOM,
+      tilt: CAMERA_TILT,
+      bearing: CAMERA_BEARING,
+      target: LatLng(currentLocation.latitude,
+          currentLocation.longitude),
+    );
+
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(cPosition));
+    // do this inside the setState() so Flutter gets notified
+    // that a widget update is due
+    setState(() {
+      // updated position
+      var pinPosition = LatLng(currentLocation.latitude,
+          currentLocation.longitude);
+
+      // the trick is to remove the marker (by id)
+      // and add it again at the updated location
+      _markers.removeWhere(
+              (m) => m.markerId.value == 'sourcePin');
+      _markers.add(Marker(
+          markerId: MarkerId('sourcePin'),
+          position: pinPosition, // updated position
+          icon: sourceIcon
+      ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+//
+//    CameraPosition initialCameraPosition = CameraPosition(
+//        zoom: CAMERA_ZOOM,
+//        tilt: CAMERA_TILT,
+//        bearing: CAMERA_BEARING,
+//        target: SOURCE_LOCATION
+//    );
+//    if (currentLocation != null) {
+//      initialCameraPosition = CameraPosition(
+//          target: LatLng(currentLocation.latitude,
+//              currentLocation.longitude),
+//          zoom: CAMERA_ZOOM,
+//          tilt: CAMERA_TILT,
+//          bearing: CAMERA_BEARING
+//      );
+//    }
+
+    return Scaffold(
+        backgroundColor: Color(0xffded3ad),
+        body:ListView(
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: GoogleMap(
+                  myLocationEnabled: true,
+                  compassEnabled: true,
+                  tiltGesturesEnabled: false,
+                  markers: _markers,
+                  polylines: _polylines,
+                  mapType: MapType.normal,
+                  initialCameraPosition: initialCameraPosition,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                    // my map has completed being created;
+                    // i'm ready to show the pins on the map
+                    showPinsOnMap();
+                  }),
+            )
+          ],
+        ),
+    );
+  }
 }
